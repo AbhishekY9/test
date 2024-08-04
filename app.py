13a511e5c704b69b5d06d9ce8c6208bc71853f45
Abhishek Yadav <148570859+AbhishekY9@users.noreply.github.com>
2024-08-04 11:13:06 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,14 +6,14 @@ import streamlit as st
 load_dotenv()
 
 def get_github_api_key():
-    return os.getenv("GITHUB_API_KEY")
+    return os.getenv("GOOGLE_API_KEY")
 
 # Streamlit app
-st.title("Display GitHub API Key")
+st.title("Display GOOGLE API Key")
 
 if st.button("Show API Key"):
     api_key = get_github_api_key()
     if api_key:
-        st.write(f"GitHub API Key: {api_key}")
+        st.write(f"GOOGLE API Key: {api_key}")
     else:
         st.write("API key not found. Please check your .env file and repository secrets.")
